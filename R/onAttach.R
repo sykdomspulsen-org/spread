6517b554496b70c3237da3209ae300cca063a3ef
Richard White <w@rwhite.no>
2019-04-14 22:15:26 +0200

    Update onAttach.R

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("PACKAGE: spread")
-  packageStartupMessage("Version 2019.04.14")
-  packageStartupMessage("Model and C++ code developed by Solveig Engebretsen and Andreas Nyg\u00E5rd Osnes")
+  packageStartupMessage("Version: 2019.04.14")
+  packageStartupMessage("Commuter model and C++ code developed by Solveig Engebretsen and Andreas Nyg\u00E5rd Osnes")
   packageStartupMessage("Ported to RCPP by Richard White")
 }
