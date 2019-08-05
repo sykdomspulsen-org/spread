9c5db6a2755001cfe884f2ed727c22a860d0e25c
Richard White <w@rwhite.no>
2019-08-05 11:05:59 +0300

    Including create_data_files_norway_2017

---
+++ R/validate.R
@@ -0,0 +1,22 @@
+validate_seiiar <- function(seiiar){
+  wanted_names <- c(
+    "location_code",
+    "S",
+    "E",
+    "I",
+    "Ia",
+    "R"
+  )
+
+  if(!all.equal(names(seiiar), wanted_names)){
+    stop(glue::glue("names are not c({glue::glue_collapse(wanted_names, sep=', ')})"))
+  }
+
+}
+
+validate_vax_prev <- function(vax){
+  needed_names <- c("location_code","proportion")
+  if(sum(!needed_names %in% names(vax))>0){
+    stop(glue::glue("names are not c({glue::glue_collapse(needed_names, sep=', ')})"))
+  }
+}
