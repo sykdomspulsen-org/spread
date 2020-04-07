4d9b7f099a5c88a45f809060f8e6b78d17e4092a
Travis CI User <travis@example.org>
2020-04-07 19:08:17 +0000

    Travis CI update 85 [ci skip]

--- R/3_onAttach.R
+++ R/3_onAttach.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("\nPACKAGE: spread")
-  packageStartupMessage("Version: 2020.04.06 at 11:22")
+  packageStartupMessage("Version: 2020.04.07 at 18:59")
   packageStartupMessage("\nCommuter model and C++ code developed by Solveig Engebretsen and Andreas Nyg\u00E5rd Osnes")
   packageStartupMessage("Asymmetric mobility model and C++ code developed by Solveig Engebretsen")
   packageStartupMessage("Commuter ported to RCPP by Richard White")
