a2e4c2da845e3d26e4f724ddca87a6d1ede12991
Richard Aubrey White <RichardAubrey.White@fhi.no>
2022-02-09 12:16:03 +0100

    onAttach

--- R/3_onAttach.R
+++ R/3_onAttach.R
@@ -1,7 +1,14 @@
 .onAttach <- function(libname, pkgname) {
+  version <- tryCatch(
+    utils::packageDescription("spread", fields = "Version"),
+    warning = function(w){
+      1
+    }
+  )
+
   packageStartupMessage(paste0(
     "spread ",
-    # utils::packageDescription("spread")$Version,
+    version,
     "\n",
     "https://docs.sykdomspulsen.no/spread"
   ))
