451999edcfcfca6b1be82b5094d809540b61d53a
Richard Aubrey White <RichardAubrey.White@fhi.no>
2022-02-09 06:40:26 +0100

    onAttach

--- R/3_onAttach.R
+++ R/3_onAttach.R
@@ -1,11 +1,8 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage(paste(
-    "spread",
-    utils::packageDescription("spread")$Version,
-    "https://folkehelseinstituttet.github.io/spread"
+  packageStartupMessage(paste0(
+    "spread ",
+    # utils::packageDescription("spread")$Version,
+    "\n",
+    "https://docs.sykdomspulsen.no/spread"
   ))
-  packageStartupMessage("\nCommuter model and C++ code developed by Solveig Engebretsen and Andreas Nyg\u00E5rd Osnes")
-  packageStartupMessage("Asymmetric mobility model and C++ code developed by Solveig Engebretsen")
-  packageStartupMessage("Commuter ported to RCPP by Richard White")
-  packageStartupMessage("Branching process developed by Gunnar Ro")
 }
